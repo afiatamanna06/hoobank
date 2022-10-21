83bfaff47c3a9fc765391ff8585c0ba32c67c70c
afiatamanna06 <afiatamanna06@gmail.com>
2022-10-21 20:07:52 +0600

    Add features section data mapping

---
+++ components/features/FeatureList.tsx
@@ -0,0 +1,23 @@
+import { FaStar, FaTelegramPlane } from "react-icons/fa"
+import { IoShieldCheckmarkSharp } from "react-icons/io5"
+
+export const featuresData = [
+    {
+      icon: <FaStar color="#5ce1e6" size={35} />,
+      title: "Rewards",
+      content:
+        "The best credit cards offer some tantalizing combinations of promotions and prizes",
+    },
+    {
+      icon: <IoShieldCheckmarkSharp color="#5ce1e6" size={35} />,
+      title: "100% Secured",
+      content:
+        "We take proactive steps make sure your information and transactions are secure.",
+    },
+    {
+      icon: <FaTelegramPlane color="#5ce1e6" size={35} />,
+      title: "Balance Transfer",
+      content:
+        "A balance transfer credit card can save you a lot of money in interest charges.",
+    },
+ ];
--- components/features/Features.tsx
+++ components/features/Features.tsx
@@ -1,5 +1,5 @@
 import GetStartedButton from "../common/GetStartedButton"
-
+import { featuresData } from "./FeatureList"
 
 function Features() {
   return (
@@ -13,6 +13,17 @@ function Features() {
                 With the right credit card, you can improve your financial life by building credit, earning rewards and saving money. But with hundreds of credit cards on the market.
             </div>
             <GetStartedButton />
+        </div>
+        <div className="flex flex-col w-full flex-1 gap-6">
+            {featuresData.map(({icon, title, content}) => (
+                <div key={title} className="flex flex-row p-2 md:p-6 rounded-[20px] gap-4">
+                    <div className="w-[3.7rem] h-[3.7rem] rounded-full bg-dimBlue p-3">{icon}</div>
+                    <div className="flex flex-col text-white">
+                        <h4 className="font-poppins font-semibold text-xl">{title}</h4>
+                        <p className="text-gray-400 font-poppins">{content}</p>
+                    </div>
+                </div>
+            ))}
         </div>
     </section>
   )
