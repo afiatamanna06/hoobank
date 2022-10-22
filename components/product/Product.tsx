830e948be82508abbd096dd15107d1e96a444792
afiatamanna06 <afiatamanna06@gmail.com>
2022-10-22 22:15:41 +0600

    Update products section

--- components/product/Product.tsx
+++ components/product/Product.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 
+import GetStartedButton from "../common/GetStartedButton"
+
 function Product() {
   return (
     <section id="product" className="flex flex-col items-center md:py-20 py-20 gap-8 lg:gap-4 px-4 md:px-10">
@@ -18,6 +20,20 @@ function Product() {
                     <img src="/google.svg" className="w-[10rem] h-[5rem] cursor-pointer" alt="" />
                 </div>
             </div>
+        </div>
+        <div className="flex flex-col items-center lg:flex-row gap-8 lg:gap-4">
+            <img src="/card.png" className="lg:hidden object-cover flex-1 md:h-[75vh] md:w-[55vw]" alt="" />
+            <div className="flex flex-col w-full flex-1 gap-6">
+                <h1 className="flex flex-col text-white md:text-5xl text-2xl font-poppins font-semibold gap-2 md:gap-6">
+                    <div>Find a better card deal</div>
+                    <div>in few easy steps.</div>
+                </h1>
+                <div className="text-gray-400 font-poppins lg:max-w-[26rem]">
+                    Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.
+                </div>
+                <GetStartedButton />
+            </div>
+            <img src="/card.png" className="hidden lg:block object-cover flex-1 md:h-[75vh] md:w-[55vw]" alt="" />
         </div>
     </section>
   )
