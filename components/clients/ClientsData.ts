c4eb3e6d35946cb6cdc03932c57ca99a3a0c7999
afiatamanna06 <afiatamanna06@gmail.com>
2022-10-23 11:17:19 +0600

    Add clients section data

---
+++ components/clients/ClientsData.ts
@@ -0,0 +1,23 @@
+export const feedback = [
+    {
+      content:
+        "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
+      name: "Herman Jensen",
+      title: "Founder & Leader",
+      img: "/people01.png",
+    },
+    {
+      content:
+        "Money makes your life easier. If you're lucky to have it, you're lucky.",
+      name: "Steve Mark",
+      title: "Founder & Leader",
+      img: "/people02.png",
+    },
+    {
+      content:
+        "It is usually people in the money business, finance, and international trade that are really rich.",
+      name: "Kenn Gallagher",
+      title: "Founder & Leader",
+      img: "/people03.png",
+    },
+];
