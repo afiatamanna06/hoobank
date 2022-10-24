74f67f9517c3ee4b80549c1da6963de65e45be27
afiatamanna06 <afiatamanna06@gmail.com>
2022-10-24 21:03:33 +0600

    Add footer social links

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable @next/next/no-img-element */
 import Link from "next/link"
 import { footerLinks } from "./FooterLinks"
+import { socialLinks } from "./SocialLinks"
 
 function Footer() {
   return (
-    <section className="flex flex-col items-center w-full py-16 gap-20 px-4 md:px-10 text-white">
+    <section className="flex flex-col items-center w-full py-16 mb-8 gap-10 px-4 md:px-10 text-white">
         <div className="flex flex-col lg:flex-row items-start justify-between w-full gap-8 lg:gap-12">
             <div className="flex flex-col gap-4">
                 <img src="/logo.svg" className="h-[80%] w-[80%]" alt="" />
@@ -43,7 +44,17 @@ function Footer() {
                 ))}
             </div>
         </div>
-
+        <div className="h-[1px] w-full bg-gray-400"></div>
+        <div className="flex flex-col lg:flex-row items-center justify-between w-full gap-8 font-poppins">
+            <p className="font-medium text-center">Copyright Ⓒ 2022 HooBank. All Rights Reserved.</p>
+            <div className="flex gap-4 items-center">
+                {socialLinks.map(({icon, link}) => (
+                    <Link href={link} key={link} passHref>
+                        <div className="hover:text-secondary cursor-pointer">{icon}</div>
+                    </Link>
+                ))}
+            </div>
+        </div>
     </section>
   )
 }
---
+++ components/footer/SocialLinks.tsx
@@ -0,0 +1,20 @@
+import { FiFacebook, FiInstagram, FiTwitter, FiLinkedin } from "react-icons/fi";
+
+export const socialLinks = [
+    {
+      icon: <FiInstagram size={20} />,
+      link: "https://www.instagram.com/",
+    },
+    {
+      icon: <FiFacebook size={20} />,
+      link: "https://www.facebook.com/",
+    },
+    {
+      icon: <FiTwitter size={20} />,
+      link: "https://www.twitter.com/",
+    },
+    {
+      icon: <FiLinkedin size={20} />,
+      link: "https://www.linkedin.com/",
+    },
+  ];
